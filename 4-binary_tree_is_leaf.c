64c7749586a29567cf0d0036489837d2c5c2ddc8
jhosep7 <jodia.ms@outlook.com>
2020-03-12 05:43:23 -0500

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!(node->left) && !(node->right) && node)
-	{return (1); }
-	return (0);
+	if (!node)
+	{return (0); }
+	return (!node->left && !node->right);
 }
